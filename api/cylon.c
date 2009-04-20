a266c23eb6a81b04962478cfcdb916f5a224487b
Russell Harmon <russ@eatnumber1.com>
2009-04-20 00:45:38 -0400

    Properly do error handling

--- api/cylon.c
+++ api/cylon.c
@@ -1,13 +1,27 @@
 #include "cylon.h"
 
-int cylon_errno;
-
-panel_t selectPanel( const panel_addr* panel ) {}
-int setDisplay( panel_t panel, const frame_t* frame ) {}
+int setDisplay( panel_t panel, const frame_t* const frame ) {}
 int setAnimation( panel_t panel, const frame_t const frame[], const int num_frames, const int_fast8_t speed ) {}
 int start( panel_t panel ) {}
 int pause( panel_t panel ) {}
 int reset( panel_t panel ) {}
 int ping( panel_t panel ) {}
+
+static char* cyl_errbuf = NULL;
+
+const char* cyl_strerror( const int errcode ) {
+	if( cyl_errbuf != NULL ) free(cyl_errbuf);
+
+	switch( errcode ) {
+		case CYL_SYSTEM:
+			cyl_errbuf = NULL;
+			return strerror(errcode);
+		default:
+			asprintf(&cyl_errbuf, "Unknown error %d", errcode);
+			break;
+	}
+	
+	return cyl_errbuf;
+}
 
 int main() {}
--- api/cylon.h
+++ api/cylon.h
@@ -1,24 +1,27 @@
 #ifndef _CYLON_H
 #define _CYLON_H
 
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
 
-typedef void* panel_t;
+#define CYL_SYSTEM -1 // Other system error, check errno for details.
 
-typedef struct panel_addr_ {
+const char* cyl_strerror( const int errcode );
+
+typedef struct panel_t_ {
 	int_fast64_t xbee;
 	int_fast8_t panel;
-} panel_addr;
+} panel_t;
 
 typedef struct frame_t_ {
 	int_fast8_t intensity;
 	int_fast16_t duration;
 } frame_t;
 
-extern int cylon_errno;
-
-panel_t selectPanel( const panel_addr* panel );
-int setDisplay( panel_t panel, const frame_t* frame );
+int setDisplay( panel_t panel, const frame_t* const frame );
 int setAnimation( panel_t panel, const frame_t const frame[], const int num_frames, const int_fast8_t speed );
 int start( panel_t panel );
 int pause( panel_t panel );
