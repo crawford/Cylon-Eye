c7057fcc800970794f29b962fea97cbb6a5737da
unknown <Alex@.(none)>
2009-12-29 22:44:57 -0500

    Added static methods for converting byte arrays to Frame. Started implementing Panel.

--- cysim/cmdproc/Frame.java
+++ cysim/cmdproc/Frame.java
@@ -1,30 +1,26 @@
 class Frame {
 	private int time;
-	private String data;
+	private String displayData;
 	private int gotoAddress;
 	
+	//inst - 6 byte string in the format TTDDDD, where TT is a time value
+	// and DDDD and is display data, or in the format 00AA00, where AA is
+	// an address
 	public Frame(byte inst[]) {
 		time = ((inst[0] & 0xFF) << 8) + (inst[1] & 0xFF);
 		
+		//Seperate the 4 bytes of data
+		byte data[] = new byte[6];
+		for(int i = 0; i < 6; i++) {
+			data[i] = inst[i + 2];
+		}
+		
 		if(time == 0) {
 			//Encoded data is an address
-			gotoAddress = ((inst[2] & 0xFF) << 8) + (inst[3] & 0xFF);
+			gotoAddress = toGotoAddress(data);
 		} else {
 			//Encoded data is display data
-		
-			//Convert last 6 bytes of inst to array of boolean
-			int bits[] = new int[48];
-			for(int b = 2; b < 8; b++) {
-				for(int p = 0; p < 8; p++) {
-					bits[(b - 2)*8 + p] = (inst[b] << p) & 0x80;
-				}
-			}
-			
-			//Convert 3-bit values to integers and store in data
-			data = "";
-			for(int i = 0; i < 16; i++) {
-				data += bits[3*i + 2]*4 + bits[3*i + 1]*2 + bits[3*i];
-			}
+			displayData = toDisplayData(data);
 		}
 	}
 	
@@ -37,6 +33,29 @@ class Frame {
 	}
 	
 	public String getDisplayData() {
-		return data;
+		return displayData;
+	}
+	
+	public static String toDisplayData(byte data[]) {
+		String displayString = "";
+		
+		//Convert the 6 bytes of data to array of booleans
+		int bits[] = new int[48];
+		for(int b = 0; b < 6; b++) {
+			for(int p = 0; p < 8; p++) {
+				bits[b*8 + p] = (data[b] << p) & 0x80;
+			}
+		}
+		
+		//Convert 3-bit values to integers and store in displayString
+		for(int i = 0; i < 16; i++) {
+			displayString += bits[3*i + 2]*4 + bits[3*i + 1]*2 + bits[3*i];
+		}
+		
+		return displayString;
+	}
+	
+	public static int toGotoAddress(byte data[]) {
+		return ((data[0] & 0xFF) << 8) + (data[1] & 0xFF);
 	}
 }
--- cysim/cmdproc/Panel.java
+++ cysim/cmdproc/Panel.java
@@ -6,11 +6,63 @@ class Panel {
 	private ArrayList<Frame> frames;
 	private String data;
 	private boolean auxLED;
+	private int pointer;
 	//private ??? timer;
 	
 	public Panel(String zid, String pid) {
 		ZID = zid;
 		PID = pid;
 		frames = new ArrayList<Frame>();
+		data = "0000000000000000";
+		auxLED = false;
+		pointer = 0;
+	}
+	
+	public void evaluate(byte command[]) {
+		//Strip the command character from the front and the newline
+		// from the back
+		byte payload[] = new byte[command.length - 2];
+		for(int i = 0; i < command.length - 2; i++) {
+			payload[i] = command[i + 1];
+		}
+			
+		switch(command[0]) {
+			case 'C':
+				//Overwrite the current display data and stop animation
+				data = Frame.toDisplayData(payload);
+				//TODO
+				break;
+			case 'L':
+				//Turn on/off the auxilary LED
+				if(payload[0] == 0) {
+					auxLED = true;
+				} else {
+					auxLED = false;
+				}
+				break;
+			case 'D':
+				//Write frame to current pointer and increment
+				frames.add(new Frame(payload));
+				pointer++;
+				break;
+			case 'X':
+				//Set pointer to specified value
+				pointer = Frame.toGotoAddress(payload);
+				break;
+			case 'S':
+				break;
+			case 'P':
+				break;
+			case 'R':
+				break;
+			case 'T':
+				break;
+			case 'N':
+				break;
+			case 'B':
+				break;
+			case 'Y':
+				break;
+		}
 	}
 }
