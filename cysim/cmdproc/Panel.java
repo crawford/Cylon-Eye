814178c3b60aca23775b1ff459e2039d1b2c9958
unknown <Alex@.(none)>
2009-12-31 14:11:02 -0500

    Added functionality for animation processing

--- cysim/cmdproc/Panel.java
+++ cysim/cmdproc/Panel.java
@@ -1,13 +1,18 @@
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 class Panel {
+	private final double TIMER_SCALE_FACTOR = 1;	// milliseconds / Panel-Time-Period
 	private String ZID;
 	private String PID;
 	private ArrayList<Frame> frames;
 	private String displayData;
 	private boolean auxLED;
 	private int pointer;
-	//private ??? timer;
+	private int period;
+	private Timer timer;
+	private int currentFrameTime;
 	
 	public Panel(String zid, String pid) {
 		ZID = zid;
@@ -16,6 +21,7 @@ class Panel {
 		displayData = "0000000000000000";
 		auxLED = false;
 		pointer = 0;
+		timer = new Timer();
 	}
 	
 	public String evaluate(byte command[]) {
@@ -29,16 +35,12 @@ class Panel {
 		switch(command[0]) {
 			case 'C':
 				//Overwrite the current display data and stop animation
-				displayData = Frame.toDisplayData(payload);
-				//TODO
+				pause();
+				setDisplay(Frame.toDisplayData(payload));
 				break;
 			case 'L':
 				//Turn on/off the auxilary LED
-				if(payload[0] == 0) {
-					auxLED = false;
-				} else {
-					auxLED = true;
-				}
+				setLED(payload[0] != 0);
 				break;
 			case 'D':
 				//Write frame to current pointer and increment
@@ -51,15 +53,21 @@ class Panel {
 				break;
 			case 'S':
 				//Start the animation from the current pointer value
+				resume();
 				break;
 			case 'R':
 				//Reset and pause the animation
+				pause();
 				pointer = 0;
 			case 'P':
 				//Pause the animation
+				pause();
 				break;
 			case 'T':
 				//Set animation speed
+				pause();
+				period = Frame.toTimeValue(payload);
+				resume();
 				break;
 			case 'N':
 				//No operation
@@ -75,5 +83,47 @@ class Panel {
 	
 	public String toString() {
 		return ZID + "-" + PID + "-" + displayData + "-" + (auxLED ? "1" : "0");
+	}
+	
+	private void pause() {
+		timer.cancel();
+	}
+	
+	private void resume() {
+		if(frames.size() > 0)
+			timer.scheduleAtFixedRate(new PanelTask(), (long)0, (long)(period*TIMER_SCALE_FACTOR));
+	}
+	
+	private void setDisplay(String data) {
+		displayData = data;
+		System.out.println(this);
+	}
+	
+	private void setLED(boolean led) {
+		auxLED = led;
+		System.out.println(this);
+	}
+	
+	private class PanelTask extends TimerTask {
+		public void run() {
+			if(currentFrameTime > 0) {
+				currentFrameTime--;
+				return;
+			}
+			
+			if(pointer >= frames.size()) {
+				pointer = 0;
+			}
+
+			Frame frame = frames.get(pointer);
+			if(frame.isGoto()) {
+				pointer = frame.getGotoAddress();
+				frame = frames.get(pointer);
+			} 
+			
+			setDisplay(frame.getDisplayData());
+			currentFrameTime = frame.getDisplayTime();
+			pointer++;
+		}
 	}
 }
